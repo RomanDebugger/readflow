8729c2f8ef5254b13716b0215187a4bfe15a4df2
Akshat Tiwari <itsakshatts@gmail.com>
2026-04-09 21:16:55 +0530

    feat:updated the model from gemini-2.5 flash to gemini-3.1 flash

--- web/app.py
+++ web/app.py
@@ -18,7 +18,7 @@ from typing import List
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-3.1-flash-lite')
 
 app = FastAPI()
 
